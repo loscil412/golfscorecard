2da1a5a54beb57cadc33832563b71c9e3268a5f1
Burkhardt Reiter <loscil@gmail.com>
2021-04-18 08:34:40 -0400

    course list data separated to individual file data.js; scorcard.js is refactor for drawing scorecard in window

--- js/scorecard.js
+++ js/scorecard.js
@@ -1,32 +1,18 @@
-const COURSE_LIST = [
-    { 
-        "name": "Edgewood Country Club",
-        "holes": { "1": 4, "2": 4, "3": 3, "4": 5, "5": 3, "6": 4, "7": 4, "8": 5, "9": 4, "10": 4, "11": 4, "12": 3, "13": 5, "14": 3, "15": 5, "16": 4, "17": 5, "18": 4 } 
-    },
-    { 
-        "name": "Bob O'Conner Muni",
-        "holes": { "1": 4, "2": 4, "3": 4, "4": 4, "5": 3, "6": 4, "7": 3, "8": 4, "9": 4, "10": 4, "11": 4, "12": 3, "13": 4, "14": 3, "15": 3, "16": 4, "17": 4, "18": 4 } 
-    },
-    { 
-        "name": "Augusta National",
-        "holes" : { "1": 4, "2": 4, "3": 3, "4": 5, "5": 3, "6": 4, "7": 4, "8": 5, "9": 4, "10": 4, "11": 4, "12": 4, "13": 3, "14": 4, "15": 5, "16": 3, "17": 4, "18": 4 } 
-    },
-    { 
-        "name": "Scally's Par 3",
-        "holes" : { "1": 3, "2": 3, "3": 3, "4": 3, "5": 3, "6": 3, "7": 3, "8": 3, "9": 3 } 
-    }
-]
-
+let selectedCourse;
 let menu = document.getElementById("menu")
-menu.addEventListener("change", courses)
+menu.addEventListener("change", drawScoreCard)
 
-function courses(){
-    let selectedCourse = COURSE_LIST[(document.querySelector('select').value) * 1]
+function getCourse(){
+    selectedCourse = COURSE_LIST[(document.querySelector('select').value) * 1]
     document.getElementById("courseSelection").innerHTML = selectedCourse.name
     document.getElementById("courseName").innerText = "Golf Score Card: " + selectedCourse.name
     if (document.getElementById("courseNameCard") != null) {
         document.getElementById("courseNameCard").innerHTML = selectedCourse.name;
     }
+}
+
+function drawScoreCard(){
+    getCourse();
     let colspanLength = Object.keys(selectedCourse.holes).length + 1
     let scorecardTable = `<table><thead><tr><th colspan=${colspanLength}>${selectedCourse.name}</th></tr></thead>`;
 
@@ -45,6 +31,7 @@ function courses(){
 
 
     document.getElementById("table").innerHTML = scorecardTable
+
 }
 
     
